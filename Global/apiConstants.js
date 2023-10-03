fe285dce1b85150570d1726c4090a86eb4275f7f
Juraj Stekla <jurajstekla@Jurajs-Laptop.local>
2023-10-03 19:29:09 +0200

    Updates

--- Global/apiConstants.js
+++
@@ -1,8 +0,0 @@
-export const apiData = () => {
-  //eslint-disable-next-line
-  return process.env.NODE_ENV === 'production' ? '.' : 'http://localhost:8080/dms';
-};
-
-export const responseStatus = {
-  400: { description: 'badCredentials' }
-};
--- api/actions/propertyActions.js
+++
@@ -1,6 +0,0 @@
-import { api } from '../api';
-
-export const addNewCustomPropertiesAction = async (dataToAdd, documentId) => {
-  const { data } = await api.post(`/api/customProperties/byDocumentId/${documentId}`, dataToAdd);
-  return data;
-};
